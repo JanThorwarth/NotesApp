a9e32a1fcaf0649948187f7b4c8a5ef8bd3a76f9
Jan <thorwarth.jan@web.de>
2025-01-03 10:45:29 +0100

    project start

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"noteapp-98636","appId":"1:1051546423371:web:f0dee32a90c5acb498fdce","storageBucket":"noteapp-98636.firebasestorage.app","apiKey":"AIzaSyBHx_y6L1VYwfTvS5-ZBvK8afNVAzJaXtA","authDomain":"noteapp-98636.firebaseapp.com","messagingSenderId":"1051546423371"})), provideFirestore(() => getFirestore())]
 };
